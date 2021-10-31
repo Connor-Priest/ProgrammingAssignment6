db37d37b062dbebd85674dbcc7d1b9f72fedbf96
Connor-Priest <cpriest965@g.rwu.edu>
2021-10-31 14:56:51 -0400

    Added command line arguments

--- main.py
+++ main.py
@@ -4,13 +4,25 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+
+
+def argumentReader():
+    print("Number of Arguments: ", len(sys.argv))
+    print("Argument List: ", str(sys.argv))
+
+
+def HelloThere():
+    f = open("HelloThere.txt", "a")
+    f.write("Hello There")
+    f.close()
+    f = open("HelloThere.txt", "r")
+    print(f.read())
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    argumentReader()
+    HelloThere()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
