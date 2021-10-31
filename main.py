4cca4e3f5cc484d1c3d2ef6f8fb948ea3e3ae65b
Connor-Priest <cpriest965@g.rwu.edu>
2021-10-31 15:57:01 -0400

    Added sentence seperations

--- main.py
+++ main.py
@@ -4,13 +4,21 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def input1():
+    sentence = input("Enter a sentence: ")
+    for words in sentence.split():
+        print(words)
+
+
+def input2():
+    sentence = input("Enter a sentence: ")
+    words = sentence.replace(" ", "\n")
+    print(words)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    input1()
+    input2()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
